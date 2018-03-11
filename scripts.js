0a74621eb55c7eec9810e935ff8b16775fdbb928
Ari Simon <arisimon2@gmail.com>
2018-03-10 17:58:02 -0800

    initial commit
    initial commit

---
+++ scripts.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyAzAoS4LK4YuMX7WMRbIhhRlt25r7hGfPA';
+
+function getDataFromApi(searchTerm, callback) {
+  let query = {
+    part: 'snippet',
+    key: API_KEY,
+    q: searchTerm,
+    maxResults: 28,
+  }
+  $.getJSON(YOUTUBE_URL, query, function(data) {
+    renderResults(data);
+  });
+}
+
+function renderResults(data) {
+  let resultVideo = '';
+  if (data.items) {
+    data.items.forEach(function(item) {
+     if (item.id.kind === 'youtube#channel') {
+        resultVideo += '<div class="result-item"><a href="https://www.youtube.com/channel/' + item.snippet.channelId + 
+        '" target="_blank"><img width="110px" height="110px" src="' + item.snippet.thumbnails.medium.url + '"><br>' + item.snippet.channelTitle + 
+        '</a></div>';
+     } else {
+        resultVideo += '<div class="result-item"><a href="https://www.youtube.com/watch?v=' + item.id.videoId + 
+        '" target="_blank"><img width="196px" height="110px" src="' + item.snippet.thumbnails.medium.url + '">' + 
+        item.snippet.title + '</a><br><span><a href="https://www.youtube.com/channel/' + item.snippet.channelId + 
+        '" target="_blank">' + item.snippet.channelTitle + '</a></span></div>'; 
+     }
+    });
+  }
+  else {
+    resultVideo += '<p>No results</p>';
+  }
+  
+  $('.js-search-results').html(resultVideo);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(e) {
+    event.preventDefault();
+    let query = $(this).find('.js-search-input').val();
+    getDataFromApi(query, renderResults);
+  });
+}
+
+watchSubmit();
